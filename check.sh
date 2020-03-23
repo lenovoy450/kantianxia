548afadf38d200207cb8a00c11d5002d06d1590a
lenovoy450 <shmily0827@tp.edu.tw>
2020-03-23 16:54:05 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,7 +2,7 @@
 ax=`vnstat --oneline | awk -F ";" '{print $11}'`
 if [[ "$ax" == *GiB* ]];
 then
- if [ $(echo "$(echo "$ax" | sed 's/ GiB//g') &amp;amp;amp;amp;amp;amp;amp;amp;amp;gt; 999"|bc) -eq 1 ]
+ if [ $(echo "$(echo "$ax" | sed 's/ GiB//g') &amp;amp;amp;amp;amp;amp;amp;amp;amp;gt; 990"|bc) -eq 1 ]
  then
  shutdown -h now
  fi
