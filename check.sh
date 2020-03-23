7fb12ac423775af01d5850cd7bea36cccd72e6ff
lenovoy450 <shmily0827@tp.edu.tw>
2020-03-23 16:16:30 +0800

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ax=`vnstat --oneline | awk -F ";" '{print $11}'`
+if [[ "$ax" == *GiB* ]];
+then
+ if [ $(echo "$(echo "$ax" | sed 's/ GiB//g') &amp;amp;amp;amp;amp;amp;amp;amp;amp;gt; 999"|bc) -eq 1 ]
+ then
+ shutdown -h now
+ fi
+fi
